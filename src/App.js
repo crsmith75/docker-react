b005e31fe3225c2f5b0f2c76fdf37fe35bfee230
Connor Smith <connorsmith@Connors-MacBook-Pro-2.local>
2019-04-10 17:19:24 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p className="App-intro">
-            I was Changed!
+            I was Changed on the feature branch!
           </p>
           <a
             className="App-link"
